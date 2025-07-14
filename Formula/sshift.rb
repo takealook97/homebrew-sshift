f45650152886d73fb288fd4b636579265fbfd235
geonwoo <geonwoo@biztalk.co.kr>
2025-07-14 17:03:58 +0900

    Update sshift to version 1.3.7

--- Formula/sshift.rb
+++ Formula/sshift.rb
@@ -1,13 +1,12 @@
 class Sshift < Formula
   desc "SSH server management tool with jump server support"
   homepage "https://github.com/takealook97/sshift"
-  version "1.3.2"
+  version "1.3.7"
   license "MIT"
-  
-  # Go source code
-  url "https://github.com/takealook97/sshift/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "8230ddfef1bf93161599605f650392badb0013cb22570f2bf807679daf3391e1"
-  
+
+  url "https://github.com/takealook97/sshift/archive/refs/tags/v1.3.7.tar.gz"
+  sha256 "c31f9eae7c0cf7dd82155630b20515db90134ad7f2251e0201d31e043e9fd44e"
+
   depends_on "go" => :build
 
   def install
@@ -27,4 +26,4 @@ class Sshift < Formula
     # Test help command
     assert_match "Usage:", shell_output("#{bin}/sshift help")
   end
-end 
+end
