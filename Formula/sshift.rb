36e5bd58786e28c14066c948e4fc27b60fe08984
takealook97 <takealook97@naver.com>
2025-07-28 20:36:54 +0900

    Update sshift to v2.1.0 with sshpass dependency

--- Formula/sshift.rb
+++ Formula/sshift.rb
@@ -1,13 +1,12 @@
 class Sshift < Formula
   desc "SSH server management tool with jump server support"
   homepage "https://github.com/takealook97/sshift"
-  version "2.0.0"
+  url "https://github.com/takealook97/sshift/archive/refs/tags/v2.1.0.tar.gz"
+  sha256 "e5a65fd345dea652c7c259fc33e5272fdc2939776c2094dfc8984206432b94d1"
   license "MIT"
 
-  url "https://github.com/takealook97/sshift/archive/refs/tags/v2.0.0.tar.gz"
-  sha256 "e5a65fd345dea652c7c259fc33e5272fdc2939776c2094dfc8984206432b94d1"
-
   depends_on "go" => :build
+  depends_on "sshpass"
 
   def install
     # Set version from git tag
@@ -15,14 +14,14 @@ class Sshift < Formula
       -s -w
       -X main.Version=#{version}
     ]
-    
+
     system "go", "build", *std_go_args(ldflags: ldflags), "-o", bin/"sshift", "main.go"
   end
 
   test do
     # Test version command
     assert_match "SSHift v#{version}", shell_output("#{bin}/sshift version")
-    
+
     # Test help command
     assert_match "Usage:", shell_output("#{bin}/sshift help")
   end
